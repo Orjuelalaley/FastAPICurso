df750acfa715d447fdd6956cfee7ad0b0f33ed9c
Sebastian Orjuela <sebasorjuela@gmail.com>
2023-10-16 12:04:20 -0500

    create more get methods

--- main.py
+++ main.py
@@ -1,8 +1,49 @@
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse, JSONResponse
 
 app = FastAPI()
+app.title = "First FastAPI App"
+app.version = "0.0.1"
+
+movies = [
+    {
+        "id": 1,
+        "title": "The Godfather",
+        "director": "Francis Ford Coppola",
+        "overview": "The aging patriarch of an organized crime dynasty transfers control of his clandestine empire to his reluctant son.",
+        "year": 1972,
+        "rating": 9.2,
+        "category": "Crime, Drama",
+    },
+    {
+        "id": 2,
+        "title": "Avengers: Endgame",
+        "director": "Francis Ford Coppola",
+        "overview": "The aging patriarch of an organized crime dynasty transfers control of his clandestine empire to his reluctant son.",
+        "year": 2019,
+        "rating": 9.8,
+        "category": "Crime, Drama",
+    },
+]
 
 
-@app.get("/")
+@app.get("/", tags=["Home Page"])
 def message():
-    return "Hello World!"
+    return HTMLResponse(content="<h1>Home Page</h1>", status_code=200)
+
+
+@app.get("/movies", tags=["Movies"])
+def get_movies():
+    return movies
+
+
+@app.get("/movies/{id}", tags=["Movies by ID"])
+def get_movie(movies_id: int):
+    if movies_id < 1:
+        # if the movie id is less than 1, return an error and status code 400
+        return JSONResponse(content={"error": "Invalid ID."}, status_code=400)
+    movie = next((movie for movie in movies if movie["id"] == movies_id), None)
+    if movie is None:
+        # if the movie id is not found, return an error and status code 404
+        return JSONResponse(content={"error": "Movie not found."}, status_code=404)
+    return movie
